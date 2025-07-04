9e5be8c014f21bb0e14fe69cb06b7eb6dbd36080
AtharvaGhorpade2405 <atharvasg24@gmail.com>
2025-07-04 11:49:06 +0530

    added token header

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,15 +1,15 @@
 function Article({ article }) {
   return (
-    <a href={article.url}>
+    <a href={article.link}>
       <div className="w-70 h-100 my-2 border-2 border-black rounded-xl overflow-y-auto scroll-hidden mx-auto md:w-80 md:h-120">
-        {article.urlToImage ? (
+        {article.image_url ? (
           <img
-            src={article.urlToImage}
+            src={article.image_url}
             className="w-full"
             alt="Image not available!"
           ></img>
         ) : (
-          <p className="text-center">Image not available!</p>
+          <p className="text-center py-2">Image not available!</p>
         )}
         <h3 className="text-xl font-semibold my-2 p-2">{article.title}</h3>
         {article.description ? (
